5145d05b2b6d067b5116ffac4e3244b2abd5575f
paloda <paloda09@gmail.com>
2017-12-23 21:11:13 +0100

    Oreo

--- device.mk
+++ device.mk
@@ -122,9 +122,9 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    SnapdragonCamera2
-    camera.msm8953 
-    libmm-qcamera 
+    Snap
+    #camera.msm8953 
+    #libmm-qcamera 
 
 # DASH technology
 PRODUCT_PACKAGES += \
