44a4345f65a814c30bbda6db0dd6f9647c432216
wenxiangli <910689331@qq.com>
2018-01-27 10:18:34 +0800

    markw:Apply LineageOS build

--- lineage.mk
+++ lineage.mk
@@ -14,17 +14,16 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/xiaomi/markw/full_markw.mk)
 
-# Inherit from markw device
-$(call inherit-product, device/xiaomi/markw/device.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Device identifier. This must come after all inclusions
-TARGET_VENDOR := Xiaomi
-PRODUCT_DEVICE := markw
 PRODUCT_NAME := lineage_markw
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 4 Prime
-PRODUCT_MANUFACTURER := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Xiaomi/markw/markw:6.0.1/MMB29M/V8.5.3.0.MBEMIED:user/release-keys" \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V8.5.3.0.MBEMIED release-keys"
